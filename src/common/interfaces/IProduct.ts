ab9778bbfe4e0591f21913db089a971be2139a10
Nhi Phan <yennhi070@gmail.com>
2022-12-14 23:05:57 +0700

    Add product data

--- src/common/interfaces/IProduct.ts
+++ src/common/interfaces/IProduct.ts
@@ -1,6 +1,13 @@
 export interface IProduct {
     id: string;
+    name: string;
     categoryId: string;
     image: string;
     price: number;
+    createdDate: Date;
+    heart: number;
+    isFeatured: boolean;
+    color: string;
+    size: string;
+    discount: number;
 }
